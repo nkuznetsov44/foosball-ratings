250213c94a0a86e49a074543c963ce2298044ffe
Nikita Kuznetsov <nkuznetsov44@gmail.com>
2022-10-25 19:49:08 +0300

    add missing files

---
+++ webapp/settings.py
@@ -0,0 +1,16 @@
+import pathlib
+from typing import Any
+
+import yaml
+
+BASE_DIR = pathlib.Path(__file__).parent
+config_path = BASE_DIR / "config.yaml"
+
+
+def get_config(path: pathlib.Path) -> dict[str, Any]:
+    with open(path) as f:
+        parsed_config = yaml.safe_load(f)
+        return parsed_config
+
+
+config = get_config(config_path)
