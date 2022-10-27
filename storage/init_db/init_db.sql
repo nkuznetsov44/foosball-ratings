7a41a92d57b8613bc473637846f2d4b906af91dc
Nikita Kuznetsov <nkuznetsov44@gmail.com>
2022-10-27 17:18:51 +0300

    add pull policy, fix initdb

--- storage/init_db/init_db.sql
+++ storage/init_db/init_db.sql
@@ -1,2 +1,2 @@
-insert into ratings_states(previous_state_id, last_competition_id)
-values (null, null);
+insert into ratings_states(previous_state_id, last_competition_id, status)
+values (null, null, 'PUBLISHED');
